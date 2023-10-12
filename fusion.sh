5c3048097c7c0b45908db21096c0da9cc7ad9f54
Maudiy <123633623+LeMaudiy@users.noreply.github.com>
2023-10-12 11:47:58 +0200

    Update fusion.sh

--- fusion.sh
+++ fusion.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Vérification des arguments
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 <fichier1> <fichier2> <fichier_sortie>"
     exit 1
 fi
 
-# Fusionner les fichiers sans doublons
 sort -u "$1" "$2" > "$3"
 
 echo "Fusion terminée. Résultat dans $3"
